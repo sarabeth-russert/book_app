bd866d4fda3bf83b4d5c3f668e9b560b645070d1
d9zero <augurson90@gmail.com>
2020-09-21 16:58:42 -0700

    progress

--- server.js
+++ server.js
@@ -16,7 +16,28 @@ app.use(express.urlencoded({extended : true}));
 
 app.get('/', renderHomePage);
 app.get('/search', renderSearchPage);
+app.post('/searches', getBookData);
 
+function getBookData (request, response) {
+  const searchQuery = request.body.search[0];
+  const searchType = request.body.search[1];
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  if(searchType === 'title'){ url += `+intitle:${searchQuery}`}
+  if(searchType === 'author'){ url += `+inauthor:${searchQuery}`}
+  console.log(request.body)
+  superagent.get(url)
+    .then(data => {
+      const bookArray = data.body.items;
+      const finalBookArray = bookArray.map(book => new Book(book.volumeInfo));
+      console.log(finalBookArray);
+      response.render('pages/searches/show', {finalBookArray: finalBookArray});
+    })
+    .catch(() => {
+      response.status(500).send('Something went wrong with your location with your superagent location');
+      console.log(url);
+    })
+
+}
 
 function renderHomePage(request, response) {
   response.render('pages/index');
@@ -25,6 +46,20 @@ function renderHomePage(request, response) {
 function renderSearchPage(request,response) {
   response.render('pages/searches/new.ejs');
 }
+
+
+function Book(volumeInfo) {
+  this.url = volumeInfo.imageLinks ? volumeInfo.imageLinks.smallThumbnail: `https://i.imgur.com/J5LVHEL.jpg`;
+  this.title = volumeInfo.title ? volumeInfo.title: ` Title Unavailable!`;
+  this.author = volumeInfo.authors ? volumeInfo.authors[0]: `Author Unavailable!`;
+  this.description = volumeInfo.description ? volumeInfo.description: `Description Not Found!?`;
+  
+}
+
+
+
+
+
 app.listen(port, () => {
   console.log('Server is listening on port', port);
 });
